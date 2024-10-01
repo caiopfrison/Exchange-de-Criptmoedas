51ec9b986c4a599a64380125f41d99e9c1aa6af6
appllei <141970214+appllei@users.noreply.github.com>
2024-10-01 23:56:45 +0000

    entrada do cpf

--- funcoes.c
+++ funcoes.c
@@ -15,3 +15,20 @@ int cadastrarUsuario(Usuario usuarios[], int *qtdUsuarios) {
     char senha[SENHA_SIZE];
 
     printf("=== Cadastro de Novo Usuário ===\n");
+
+    
+// Entrada do CPF
+    while (1) {
+        printf("CPF (apenas números, 11 dígitos): ");
+        scanf("%s", cpf);
+
+        // Verificar se o CPF já existe
+        if (encontrarUsuario(usuarios, *qtdUsuarios, cpf) != -1) {
+            printf("CPF já cadastrado. Tente novamente.\n");
+        } else if (strlen(cpf) != 11) {
+            printf("CPF inválido. Deve conter exatamente 11 dígitos.\n");
+        } else {
+            break;
+        }
+    }
+    strcpy(novoUsuario.cpf, cpf);
